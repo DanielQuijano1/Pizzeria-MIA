5116314521205b5b2fa86b5ee618a857c0dd57b4
Daniel Quijano <hector.daniel.quijano@gmail.com>
2022-12-03 16:46:04 -0300

    diseño header

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,21 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+
+let navegadorDropdown = document.querySelector(".seccion1");
+
+navegadorDropdown.onclick = function(){
+    navegadorDropdown.classList.toggle("active1")
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.seccion__etiqueta')) {
-    var dropdowns = document.getElementsByClassName("opcion");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+
+let navegadorDropdown2 = document.querySelector(".seccion2");
+
+navegadorDropdown2.onclick = function(){
+    navegadorDropdown2.classList.toggle("active2")
 }
 
-function myFunction1() {
-  document.getElementById("myDropdown1").classList.toggle("show");
-}
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.seccion__etiqueta')) {
-    var dropdowns = document.getElementsByClassName("opcion");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+let navegadorDropdown3 = document.querySelector(".seccion3");
 
-function myFunction2() {
-  document.getElementById("myDropdown2").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.seccion__etiqueta')) {
-    var dropdowns = document.getElementsByClassName("opcion");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+navegadorDropdown3.onclick = function(){
+    navegadorDropdown3.classList.toggle("active3")
 }
 
